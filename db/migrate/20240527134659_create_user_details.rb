1a19cee369d4ccd32a83720b77d86854fdf5d334
nashthecoder <naijeria@gmail.com>
2024-06-10 01:17:30 +0300

    fixed migration file error

--- db/migrate/20240527134659_create_user_details.rb
+++ db/migrate/20240527134659_create_user_details.rb
@@ -1,12 +1,11 @@
-# class CreateUserDetails < ActiveRecord::Migration[7.1]
-#   def change
-#     create_table :user_details do |t|
-#       t.string :name
-#       t.string :linkedin
-#       t.string :contribution
-#       t.text :content
-
-#       t.timestamps
-#     end
-#   end
-# end
+class CreateUserDetails < ActiveRecord::Migration[7.1]
+  def change
+    create_table :user_details do |t|
+      t.string :name
+      t.string :contact_details
+      t.text :contributions, default: "[]", null: false
+      t.string :contact_options
+      t.timestamps
+    end
+  end
+end
