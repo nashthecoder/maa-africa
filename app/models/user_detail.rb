a5f06c0e6220d93076e9c0e054d5ae31c6ab2577
nashthecoder <naijeria@gmail.com>
2024-06-09 23:33:20 +0300

    added validation to user detail model

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,4 +1,7 @@
 class UserDetail < ApplicationRecord
     default_scope { where.not(contributions: []) }  # Ensures contributions is never empty
     validates :contributions, presence: true
+    validates :contact_options, presence: true
+    validates :contact_details, presence: true
+    validates :name, presence: true # Optional validation for contact_options
 end
