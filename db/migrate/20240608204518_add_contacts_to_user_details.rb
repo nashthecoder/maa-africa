094f67b4523cde3c0438c00ce403abf3e8dcf8db
nashthecoder <naijeria@gmail.com>
2024-06-10 01:19:46 +0300

    fixed migration file error

--- db/migrate/20240608204518_add_contacts_to_user_details.rb
+++ db/migrate/20240608204518_add_contacts_to_user_details.rb
@@ -1,5 +1,5 @@
-# class AddContactsToUserDetails < ActiveRecord::Migration[7.1]
-#   def change
-#     add_column :user_details, :contacts, :string
-#   end
-# end
+class AddContactsToUserDetails < ActiveRecord::Migration[7.1]
+  def change
+    add_column :user_details, :contacts, :string
+  end
+end
