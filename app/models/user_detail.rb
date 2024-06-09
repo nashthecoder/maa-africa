5122be40acfcb9dc312da655c6cdb788321f331c
nashthecoder <naijeria@gmail.com>
2024-06-10 00:51:55 +0300

    fixing deployment errors in the user model.

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,5 +1,5 @@
 class UserDetail < ApplicationRecord
-    default_scope { where.not(contributions: []) }  # Ensures contributions is never empty
+    # default_scope { where.not(contributions: []) }  # Ensures contributions is never empty
     validates :contributions, presence: true
     validates :contact_details, presence: true
     validates :name, presence: true
