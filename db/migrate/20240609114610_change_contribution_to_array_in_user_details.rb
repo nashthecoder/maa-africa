e5574dd9635cd55b3c19e62924ef93c816f6c1cc
nashthecoder <naijeria@gmail.com>
2024-06-09 15:43:09 +0300

    ran migration file

--- db/migrate/20240609114610_change_contribution_to_array_in_user_details.rb
+++ db/migrate/20240609114610_change_contribution_to_array_in_user_details.rb
@@ -1,6 +1,6 @@
 class ChangeContributionToArrayInUserDetails < ActiveRecord::Migration[6.0]
   def up
-    add_column :user_details, :contributions, :string, array: true, default: [], null: false
+    add_column :user_details, :contributions, :string, array: true, default: '{}', null: false
     UserDetail.reset_column_information
     UserDetail.find_each do |user_detail|
       user_detail.update(contributions: [user_detail.contribution])
