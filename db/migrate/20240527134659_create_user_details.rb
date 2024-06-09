66e3682e5721b0d47d6ace9d4f1b48379a6d9569
nashthecoder <naijeria@gmail.com>
2024-06-10 00:57:48 +0300

    deleted migration files

--- db/migrate/20240527134659_create_user_details.rb
+++ db/migrate/20240527134659_create_user_details.rb
@@ -1,12 +1,12 @@
-class CreateUserDetails < ActiveRecord::Migration[7.1]
-  def change
-    create_table :user_details do |t|
-      t.string :name
-      t.string :linkedin
-      t.string :contribution
-      t.text :content
+# class CreateUserDetails < ActiveRecord::Migration[7.1]
+#   def change
+#     create_table :user_details do |t|
+#       t.string :name
+#       t.string :linkedin
+#       t.string :contribution
+#       t.text :content
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
